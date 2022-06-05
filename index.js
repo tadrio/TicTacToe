575a51e7b1057eaba7b61789e2deca1acb9634a4
Tadrio Tabosa <tadrio_88@hotmail.com>
2022-06-05 16:22:12 -0300

    Finished
    Finished the game adding the BOT function and the Best of 3 or 5 working.
    Minor upgrades and fixes.

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ const $player1Name = document.querySelector(".left-panel__input1");
 const $player2Name = document.querySelector(".left-panel__input2");
 const $resetButton = document.querySelector(".left-panel__restart-button");
 const $moveHistory = document.querySelector(".right-panel__history-box");
+const $gameHistory = document.querySelector(".left-panel__history-box");
+const $botSwitchBorder = document.querySelector(
+  ".left-panel__bot-switcher-outline"
+);
+const $botSwitchBall = document.querySelector(
+  ".left-panel__bot-switcher--ball"
+);
+const $roundsSwitchBorder = document.querySelector(
+  ".left-panel__round-numbers-config__rounds-switcher-outline"
+);
+const $roundsSwitchBall = document.querySelector(
+  ".left-panel__round-numbers-config__rounds-switcher-ball"
+);
+const $winnerbestof = document.querySelector(".winner-bestof");
 
 const winCondition = [
   [0, 1, 2],
@@ -27,10 +41,112 @@ const winCondition = [
 
 let playerMove = "X";
 let currentPlayer = $player1Name.value;
-let gameStart = false;
+let canPlay = false;
 let winner = "";
 let scoreP1 = 0;
 let scoreP2 = 0;
+let winnerName = "";
+let botActive = false;
+let player2Name = "";
+let bestOf = 3;
+
+function toggleBestOf() {
+  bestOf = bestOf === 3 ? 5 : 3;
+}
+
+function verifyBestOf() {
+  let gameWinner = '';
+  let minMatches = Math.round(bestOf / 2);
+
+  if (scoreP1 >= minMatches || scoreP2 >= minMatches) {
+    gameWinner = scoreP1 > scoreP2 ? $player1Name.value : $player2Name.value;
+    canPlay = false;
+    printGameWinner(gameWinner);
+  }
+}
+
+function printGameWinner(gameWinner) {
+  $winnerbestof.innerHTML = `
+  <br>
+  <span class="mid-panel__scoreboard__title">
+  ${gameWinner} venceu a melhor de ${bestOf}
+  </span>`;
+  setTimeout(resetGame, 1000);
+
+};
+
+function resetGame(){
+  scoreP1 = 0;
+  scoreP2 = 0;
+  resetScoreBoard();
+  resetBoard();
+  resetMoveHistory();
+  resetGameHistory();
+  resetGameWinner();
+}
+
+function resetGameWinner(){
+  $winnerbestof.innerHTML = ``;
+}
+
+function bot() {
+  if (
+    verifyMatch() === "X" ||
+    verifyMatch() === "O" ||
+    verifyMatch() === "draw"
+  )
+    return;
+
+  const index = Math.floor(Math.random() * 9);
+  const fieldItem = $board[index];
+  const gameStatus = verifyMatch();
+  if (fieldItem.textContent != "" && gameStatus != "draw") return bot();
+  canPlay = false;
+  move(index, "bot");
+}
+
+function move(field, playerType) {
+  const fieldItem = $board[field];
+
+  if (!canPlay && playerType === "player") return;
+  if (fieldItem.textContent !== "") return;
+
+  fieldItem.innerHTML = playerMove;
+
+  const gameResult = verifyMatch();
+  const scenery = getScenery();
+
+  if (gameResult === "X" || gameResult === "O") {
+    canPlay = false;
+    addPoint(gameResult);
+    setTimeout(resetBoard, 1000);
+    playerMove = "O";
+    printWinnerName(gameResult);
+    printGameHistory(winnerName, scenery);
+    setTimeout(resetMoveHistory, 1000);
+  }
+
+  if (gameResult === "draw") {
+    canPlay = false;
+    setTimeout(resetBoard, 800);
+    playerMove = "O";
+    printWinnerName(gameResult);
+    printGameHistory(winnerName, scenery);
+    setTimeout(resetMoveHistory, 1000);
+  }
+
+  printMoveHistory(playerMove, currentPlayer, field);
+  toggleMove();
+  printScore();
+  verifyBestOf();
+  if (playerType === "player" && botActive) {
+    setTimeout((canPlay = false));
+    setTimeout(bot, 250);
+  }
+  if (playerType === "bot") {
+    setTimeout((canPlay = true));
+  }
+}
 
 function toggleMove() {
   if (playerMove === "X") {
@@ -49,10 +165,13 @@ function printScore() {
 
 function printWinnerName(winner) {
   if (winner === "X") {
-    $winnerName.textContent = `${$player1Name.value} venceu!`;
+    winnerName = $player1Name.value;
+    $winnerName.textContent = `${winnerName} venceu!`;
   } else if (winner === "O") {
-    $winnerName.textContent = `${$player2Name.value} venceu!`;
+    winnerName = $player2Name.value;
+    $winnerName.textContent = `${winnerName} venceu!`;
   } else {
+    winnerName = "Empate";
     $winnerName.textContent = "Empate";
   }
 }
@@ -67,21 +186,45 @@ function printMoveHistory(move, player, field) {
     "Linha 2 | Coluna 3",
     "Linha 3 | Coluna 1",
     "Linha 3 | Coluna 2",
-    "Linha 3 | Coluna 3"
+    "Linha 3 | Coluna 3",
   ];
 
-  $moveHistory.innerHTML +=
-  `<div class="right-panel__history-box__history-wrapper">
+  $moveHistory.innerHTML += `
+  <div class="right-panel__history-box__history-wrapper">
       <p class="right-panel__history-box__history-wrapper--move-text">${move}</p>
     <div class="right-panel__history-box__history-wrapper--info">
       <p class="right-panel__history-box__history-wrapper--info-player">${player}</p>
       <p class="right-panel__history-box__history-wrapper--info-location">${dictionaryIndex[field]}</p>
     </div>
-   </div>`;
+  </div>`;
 }
 
-function printGameHistory(player, ){
+function getScenery() {
+  const scenery = [];
+  for (const $field of $board) {
+    scenery.push($field.textContent);
+  }
+  return scenery;
+}
 
+function printGameHistory(winner, scenery) {
+  let miniBoard = "";
+  for (const move of scenery) {
+    miniBoard += `<div class="left-panel__history-box__history-wrapper--scenario--squares">${move}</div>`;
+  }
+
+  $gameHistory.innerHTML += `
+  <div class="left-panel__history-box__history-wrapper">
+    <div class="left-panel__history-box__history-wrapper--winner-wrapper">
+      <strong class="left-panel__history-box__history-wrapper--winner-wrapper--title">Vencedor</strong>
+      <span class="left-panel__history-box__history-wrapper--winner-wrapper--name">
+      ${winner}</span>
+    </div>
+    <span class="left-panel__history-box__history-wrapper--text">Cenário</span>
+    <div class="left-panel__history-box__history-wrapper--scenario">
+    ${miniBoard}
+    </div>
+  </div>`;
 }
 
 function addPoint(winner) {
@@ -118,7 +261,7 @@ function resetBoard() {
     $board[i].textContent = "";
   }
   $winnerName.textContent = "";
-  gameStart = true;
+  canPlay = true;
 }
 
 function resetScoreBoard() {
@@ -131,55 +274,43 @@ function resetMoveHistory() {
 }
 
 function resetGameHistory() {
-  //reset Histórioco de Partidas
+  $gameHistory.innerHTML = "";
 }
 
 $playButton.addEventListener("click", function () {
-  if (!gameStart) {
-    gameStart = true;
+  if (!canPlay) {
+    canPlay = true;
     $playButton.innerHTML = "Pausar";
   } else {
-    gameStart = false;
+    canPlay = false;
     $playButton.innerHTML = "Iniciar";
   }
   $playButton.classList.toggle = "button-paused";
 });
 
 $resetButton.addEventListener("click", function () {
-  resetScoreBoard();
-  resetBoard();
-  resetMoveHistory()
-  //reset historico de partidas
+  resetGame();
 });
 
 $board.forEach(function ($field, field) {
   $field.addEventListener("click", function () {
-    if (!gameStart) return;
-    if ($field.textContent !== "") return;
-
-    $field.innerHTML = playerMove;
-
-    const gameResult = verifyMatch();
-
-    if (gameResult === "X" || gameResult === "O") {
-      gameStart = false;
-      addPoint(gameResult);
-      setTimeout(resetBoard, 1000);
-      playerMove = "O";
-      printWinnerName(gameResult);
-      setTimeout(resetMoveHistory, 1000);
-    }
-
-    if (gameResult === "draw") {
-      gameStart = false;
-      setTimeout(resetBoard, 800);
-      playerMove = "O";
-      printWinnerName(gameResult);
-      setTimeout(resetMoveHistory, 1000);
-    }
-    printMoveHistory(playerMove, currentPlayer, field)
-    printScore();
-    toggleMove();
+    move(field, "player");
   });
 });
-//resetmovehistory
+
+$botSwitchBorder.addEventListener("click", function () {
+  $botSwitchBall.classList.toggle("active");
+  botActive = !botActive;
+  if ($player2Name.value === "BOT") {
+    $player2Name.value = player2Name;
+  } else {
+    player2Name = $player2Name.value;
+    $player2Name.value = "BOT";
+  }
+  $player2Name.disabled = !$player2Name.disabled;
+});
+
+$roundsSwitchBorder.addEventListener("click", function () {
+  $roundsSwitchBall.classList.toggle("active");
+  toggleBestOf();
+});
